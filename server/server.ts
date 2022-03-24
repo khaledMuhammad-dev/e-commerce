95e2d852531225769008372b6d06a4ce5f535cae
Khaled Muhammad <khaledmuhammad.dev@gmail.com>
2022-03-24 15:34:23 +0200

    server init

---
+++ server/server.ts
@@ -0,0 +1,21 @@
+import express from "express";
+import cors from "cors";
+import router from "./routes/routes";
+
+const app = express();
+const log = console.log;
+
+
+app.use(cors())
+app.use(express.json());
+
+
+app.use("/", router);
+
+
+const port = 8000;
+
+app.listen(port, () => {
+    console.log(`listening on prot ${ port }`);
+});
+
