f6042eccb66466e936a24f17a2e88a99e3f53ad6
Khaled Muhammad <khaledmuhammad.dev@gmail.com>
2022-03-27 18:02:51 +0200

    chore: add mongoose package

---
+++ server/db/connect.ts
@@ -0,0 +1,8 @@
+
+/**
+ * MongoDB
+ * username: ecommerce
+ * password: a3jOrOJETOQ8Tx6j
+ */
+
+const db = `mongodb+srv://e-commerce:a3jOrOJETOQ8Tx6j@free.czv5l.mongodb.net/e-commerce?retryWrites=true&w=majority`
