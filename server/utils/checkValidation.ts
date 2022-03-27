f8cdf4ebc6b018ba1f67907c9065c4717a597eb6
Khaled Muhammad <khaledmuhammad.dev@gmail.com>
2022-03-27 16:46:22 +0200

    doc/feat: add utils folder/check uploaded images validation

---
+++ server/utils/checkValidation.ts
@@ -0,0 +1,37 @@
+import { UploadedFile } from "express-fileupload";
+
+export function checkValidation(imgs: UploadedFile[]) {
+    const maxSize = 1024 * 1024 * 4; // 4MB
+    const errors: Errors = {
+        type_error: [],
+        size_error: []
+    };
+
+    imgs.forEach(img => {
+
+        // check file type
+        if( img.mimetype.startsWith("image/jpeg") || img.mimetype.startsWith("image/png") ) {
+            // Do Nothing
+        } else {
+            errors.type_error.push( img.name );
+        }
+
+        // check file size
+        if( img.size > maxSize ) {
+            errors.size_error.push(img.name);
+        }
+    });
+
+
+   if(errors.size_error.length || errors.type_error.length) {
+       return [false, errors];
+   }
+
+   return [true];
+}
+
+
+interface Errors {
+    type_error: Array<string>
+    size_error: Array<string>
+}
