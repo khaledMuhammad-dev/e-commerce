d39562ad43130dd73592955efb579ad7d01a08c0
Khaled Muhammad <khaledmuhammad.dev@gmail.com>
2022-03-28 10:12:45 +0200

    feat: invoke function to start the server and the database connection

--- server/server.ts
+++ server/server.ts
@@ -4,7 +4,7 @@ import cors from "cors";
 import wishListRouter from "./routes/wishlist.route";
 import cartRouter from "./routes/cart.route";
 import productRouter from './routes/product.route';
-
+import connectDB, { DB_URL } from "./db/connect";
 
 const app = express();
 const log = console.log;
@@ -12,7 +12,7 @@ const log = console.log;
 app.use(cors());
 app.use(express.json());
 
-app.use(fileUpload())
+app.use(fileUpload());
 
 app.use( express.static("./public") );
 /** Product Routes */
@@ -26,8 +26,22 @@ app.use("/api/v1/cart", cartRouter);
 app.use("/api/v1/wishlist", wishListRouter);
 
 
-const port = 8000;
+const port = process.env.PORT || 8000;
 
-app.listen(port, () => {
-    console.log(`listening on prot ${ port }`);
-});
+const startConnection = async () => {
+
+    try {
+        await connectDB( DB_URL );
+
+        app.listen(port, () => {
+            console.log(`listening on prot ${ port }`);
+        });
+
+    } catch ( error ) {
+        console.log( error );
+    }
+
+}
+
+
+startConnection();
