b876162bd42c880e332dae558f723ea8c13a8333
Khaled Muhammad <khaledmuhammad.dev@gmail.com>
2022-03-28 10:10:19 +0200

    feat: added mongoose functio to connect the database

--- server/db/connect.ts
+++ server/db/connect.ts
@@ -3,6 +3,14 @@
  * MongoDB
  * username: ecommerce
  * password: a3jOrOJETOQ8Tx6j
+ * 
  */
+import mongoose from "mongoose";
 
-const db = `mongodb+srv://e-commerce:a3jOrOJETOQ8Tx6j@free.czv5l.mongodb.net/e-commerce?retryWrites=true&w=majority`
+const connectDB = ( url: string ) => {
+    return mongoose.connect( url );
+}
+
+
+export default connectDB;
+export const DB_URL = `mongodb+srv://e-commerce:a3jOrOJETOQ8Tx6j@free.czv5l.mongodb.net/e-commerce?retryWrites=true&w=majority`;
