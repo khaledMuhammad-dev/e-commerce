9cf45e27b5a6c0cb8808c0ba8cf4e463a5f38403
Khaled Muhammad <khaledmuhammad.dev@gmail.com>
2022-03-28 10:08:37 +0200

    refactor: refactor the code to handle database

--- server/controllers/product.controller.ts
+++ server/controllers/product.controller.ts
@@ -2,76 +2,55 @@
 import { Request, Response } from "express";
 import { StatusCodes } from "http-status-codes";
 import { UploadedFile } from "express-fileupload";
+import product from "../model/product";
 import { checkValidation } from "../utils/checkValidation";
 import path from "path";
-// interface Product {
-//     title: string,
-//     id: 2,
-//     description: string,
-//     price: number,
-//     imgs: string, // Todo: Array<string> for later refactore
-//     available_pieces: number,
-//     category: string, // "electronics"
-//     type: string, // Mobile, TV, etc..
-//     // rate: number, //* not required
-//     // reviews_count: number, //* not required
-//     discount: number, //* not required
-//     specifications: {
-//         brand: string,
-//         color: string
-//     }
-// }
-
-interface Product {
-    _id: string
-}
 
 
-/**
- **  Category **
- ** ---------------
- *  - Books
- *  - Sports
- *  - Electronics
- *  - Children and babies
- *  - Style and fashion
- *  - Pets
- *  - Beauty and Health
- */ 
+export async function addProduct( req: Request, res: Response ) {
+    const result = await product.create( req.body );
 
-const products:Array<Product> = [];
+    try {
+        
+        res.status( StatusCodes.CREATED ).json({ data: result, status: "SUCCESS"});
 
-export function addProduct( req: Request, res: Response ) {
-    const date = new Date();
-    req.body._id = date.getTime().toString(16);
+    } catch( error ) {
 
-    products.push( req.body );
-    res.status( StatusCodes.OK ).json({ data: req.body, Len_Now: products.length, status: "New Item just Added! 🚀"});
-
-}
-
-export function getAllProducts(req: Request, res: Response ) {
-
-    res.status( StatusCodes.OK ).json({ products, Len_Now: products.length, status: "Here is what you need baby! 😘"});
-}
-
-export function updateProduct(req: Request, res: Response ) {
-    const { _id } = req.body;
-
-    const index = products.findIndex(item => item._id === _id);
-
-    if(index < 0) {
-        res.status( StatusCodes.NOT_FOUND ).json({ error: "ITEM_NOT_EXIST" });
+        console.log(error);
+        res.status( StatusCodes.INTERNAL_SERVER_ERROR ).json({ status: "FIALD", error });
     }
-    products.splice(index,1, req.body);
-    res.status( StatusCodes.OK ).json({ data: req.body, status: "Success Proccess 🌽" });
+
+}
+
+export async function getAllProducts(req: Request, res: Response ) {
+
+    const result = await product.find({});
+
+    try {
+        res.status( StatusCodes.OK ).json({ data: result, status: "SUCCESS"});
+
+    } catch ( error ) {
+        res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ status: "FAILD", error });
+    }
+}
+
+export async function updateProduct( req: Request, res: Response ) {
+    const { _id } = req.params;
+    const singleProduct = await product.findOneAndUpdate({ _id }, req.body);
+
+    try {
+        res.status( StatusCodes.OK ).json({ data: singleProduct, status: "SUCCESS" });
+
+    } catch ( error ) {
+        res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({status: "FAILD", error});
+    }
 }
 
 export async function uploadImage( req: Request, res: Response ) {
     
     if(!req.files) {
         
-        res.status( StatusCodes.BAD_REQUEST ).json({ status: "FAILD!" });
+        res.status( StatusCodes.BAD_REQUEST ).json({ error: "images field is required" ,status: "FAILD" });
         return;
     }
 
@@ -86,7 +65,7 @@ export async function uploadImage( req: Request, res: Response ) {
     if ( !isVlalid[0] ) {
         
         res.status( StatusCodes.BAD_REQUEST )
-           .json({ errors: isVlalid[1], status: "BAD_REQUEST 😞"});
+           .json({ errors: isVlalid[1], status: "FAILD"});
         return;
     }
      
@@ -103,39 +82,34 @@ export async function uploadImage( req: Request, res: Response ) {
     }
 
     res.status( StatusCodes.OK )
-       .json({ images_urls: paths , status: "File Uploaded Successfully! 🌽" });
+       .json({ images_urls: paths , status: "SUCCESS" });
 }
 
-export function removeProduct(req: Request, res: Response ) {
+export async function destroyProduct(req: Request, res: Response ) {
+    const { _id } = req.params;
+    const result = await product.findOneAndDelete({ _id });
 
-    const { _id } = req.body;
-    const index = products.findIndex(item => item._id == _id);
+    try {
 
-    if(index < 0) {
-
-        res.status( StatusCodes.NOT_FOUND ).json({ error: "PRODUCT_NOT_FOUND" });
-        return;
+        res.status( StatusCodes.OK ).json({ data: result, status: "SUCCESS"});
+    } catch (error) {
+        
+        res.status( StatusCodes.INTERNAL_SERVER_ERROR ).json({ status: "FAILD", error});
     }
 
-    products.splice(index,1);
-    res.status( StatusCodes.OK )
-       .json({ Len_Now: products.length ,status: "PRODUCT_DELETED ❌" });
 }
 
-export function getSingleProduct(req: Request, res: Response ) {
+export async function getSingleProduct(req: Request, res: Response ) {
 
     const { _id } = req.params;
+    const result = await product.findById(_id);
 
-    const index = products.findIndex(item => item._id === _id);
+    try {
+        res.status(StatusCodes.OK).json({ data: result, status: "SUCCESS" });
 
-    if(index < 0) {
+    }catch (error) {
+        res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ status: "FAILD",  error});
 
-        res.status(StatusCodes.NOT_FOUND).json({ status: "Not_FOUND 🕵️‍♀️" })
-        return;
     }
-
-    const singleProduct = products[index];
-    res.status(StatusCodes.OK)
-       .json({ data: singleProduct, status: "SUCCESS OPERATION 🌽" })
 
 }
