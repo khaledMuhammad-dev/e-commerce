6c0ec8b7f7c2f7be7f68c1a41a49e87243a260e0
Khaled Muhammad <khaledmuhammad.dev@gmail.com>
2022-04-04 10:29:49 +0200

    add next function type

--- server/middleware/errorHandler.ts
+++ server/middleware/errorHandler.ts
@@ -1,14 +1,16 @@
-import { Request, Response } from "express";
+import { Request, Response, NextFunction } from "express";
 import CustomAPIError from "../errors/custom-error";
 import { StatusCodes } from "http-status-codes";
 
+/** State Error */
 function errorHandlerMiddleware(
     err: any, 
     req: Request, 
     res: Response, 
-    next: () => void
+    next: NextFunction
 ) {
 
+    /** State Error */
     const customError = {
         error: {
             message: err.message || "Somthing went wrong",
@@ -19,32 +21,30 @@ function errorHandlerMiddleware(
 
 
     /** Custom Errors */
-    if(err instanceof CustomAPIError) {
+    if( err instanceof CustomAPIError ) {
         customError.error.message = err.message;
         customError.statusCode = err.statusCode;
     }
 
     /** Catch Mongoose Errors */
-    if(err.name === "CastError") {
+    if( err.name === "CastError" ) {
         customError.error.message = `Invalid ${ err.path }: ${ err.value }`;
         customError.statusCode = StatusCodes.BAD_REQUEST;
     }
 
-    if(err.errors){
+    if( err.errors ){
         const key = Object.keys(err.errors)[0];
         const errorAPI = err.errors[key];
-        const message = errorAPI.properties.message;
-
+        
         // set the cutomError
-        customError.error.message = message;
+        customError.error.message = `Invalid ${ errorAPI.path }: ${ errorAPI.value }`;
         customError.statusCode = StatusCodes.BAD_REQUEST;
-
     }
 
-    if(err.type === "entity.parse.failed") {
-        
+    if( err.type === "entity.parse.failed" ) {
         customError.error.message = "Invalid JSON is Received";
         customError.statusCode = StatusCodes.BAD_REQUEST;
+
     }
 
     return res.status(customError.statusCode).json(customError.error);
